d38e12e994badbe34be6402522e21d2522c041ac
HitanshiJain01 <hitanshij43@gmail.com>
2024-07-24 12:13:12 +0530

    Changes in header responsive, custom.js, styles: header, testimonial

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -163,3 +163,20 @@ $(document).ready(function () {
     });
 
 });
+
+
+// MOBILE MENU
+$(document).ready(function () {
+    $(".menu-btn a").click(function () {
+        $(".nav").fadeToggle(200);
+        $(this).toggleClass('btn-open').toggleClass('btn-close');
+    });
+    $('.nav').on('click', function () {
+        $(".nav").fadeToggle(200);
+        $(".menu-btn a").toggleClass('btn-open').toggleClass('btn-close');
+    });
+    $('.menu__lists a').on('click', function () {
+        $(".nav").fadeToggle(200);
+        $(".menu-btn a").toggleClass('btn-open').toggleClass('btn-close');
+    });
+});
